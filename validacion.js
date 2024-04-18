0ed3c7fd29140b836fb573b076d31edd6bbea2e2
Juan-DavidP <juandavidpelaezvilla@gmail.com>
2024-04-18 11:20:25 -0500

    mobile first

--- validacion.js
+++ validacion.js
@@ -11,21 +11,49 @@ function validacionNombre() {
     let styleInput = inputNombre.style.cssText = 'border: 1px red solid;';
     let displayBlock = alertaNombre.style.display = 'block';
 
-    if ((inputNombre.value).length == 0) {
+    if ((inputNombre.value).trim().length == 0) {
         // inputNombre.style.cssText = 'border: 1px red solid;';
         styleInput;
         // alertaNombre.style.display = 'block';
         displayBlock;
         alertaNombre.innerText = "El campo nombre no puede estar vacío."
-    } else if ((inputNombre.value).length > 50) {
+        return inputNombre.value;
+    } else if ((inputNombre.value).trim().length > 50) {
         styleInput;
         // alertaNombre = document.getElementById('advertencia__nombre');
         displayBlock;
         alertaNombre.innerText = "El campo nombre no puede superar los 50 caracteres.";
+        return inputNombre.value;
     }
     else {
         inputNombre.style.cssText = 'border: none';
         alertaNombre.style.display = 'none';
+        return inputNombre.value;
+    }
+
+}
+
+function validacionEmail() {
+    const alertaEmail = document.getElementById('advertencia__email');
+    let styleInput = inputEmail.style.cssText = 'border: 1px red solid;';
+    let displayBlock = alertaEmail.style.display = 'block';
+    const expRegCorreo = /[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,5}/;
+
+    if ((inputEmail.value).trim().length == 0) {
+        styleInput;
+        displayBlock;
+        alertaEmail.innerText = "El campo Email no puede estar vacío."
+        return inputEmail.value;
+    } else if (!expRegCorreo.test(inputEmail.value.trim())) {
+        displayBlock;
+        styleInput;
+        alertaEmail.innerText = "El correo ingresado no es valido.";
+        return inputEmail.value;
+    }
+    else {
+        inputEmail.style.cssText = 'border: red';
+        alertaEmail.style.display = 'none';
+        return inputEmail.value;
     }
 
 }
@@ -35,45 +63,24 @@ function validacionAsunto() {
     let styleInput = inputAsunto.style.cssText = 'border: 1px red solid;';
     let displayBlock = alertaAsunto.style.display = 'block';
 
-    if ((inputAsunto.value).length == 0) {
+    if ((inputAsunto.value).trim().length == 0) {
         // inputAsunto.nextElementSibling.classList.add('formcontato__advertencia');
         // inputAsunto.nextElementSibling.innerText = "El campo asunto no puede estar vacío.";
         styleInput;
         displayBlock;
         alertaAsunto.innerText = "El campo asunto no puede estar vacío.";
-    } else if ((inputAsunto.value).length > 50) {
+        return inputAsunto.value;
+    } else if ((inputAsunto.value).trim().length > 50) {
         styleInput;
         displayBlock;
         alertaAsunto.innerText = "El campo asunto no puede superar los 50 caracteres.";
+        return inputAsunto.value;
     }
     else {
         inputAsunto.style.cssText = 'border: none';
         alertaAsunto.style.display = 'none';
+        return inputAsunto.value;
     }
-}
-
-
-function validacionEmail() {
-    const alertaEmail = document.getElementById('advertencia__email');
-    let styleInput = inputEmail.style.cssText = 'border: 1px red solid;';
-    let displayBlock = alertaEmail.style.display = 'block';
-    const expRegCorreo = /[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,5}/;
-
-    if ((inputEmail.value).length == 0) {
-        styleInput;
-        displayBlock;
-        alertaEmail.innerText = "El campo Email no puede estar vacío."
-    } else if (!expRegCorreo.test(inputEmail.value)) {
-        displayBlock;
-        styleInput;
-        alertaEmail.innerText = "El correo ingresado no es valido.";
-    }
-    else {
-        inputEmail.style.cssText = 'border: red';
-        alertaEmail.style.display = 'none';
-
-    }
-
 }
 
 function validacionMensaje() {
@@ -85,23 +92,19 @@ function validacionMensaje() {
         styleInput;
         displayBlock;
         alertaMensaje.innerText = "El campo mensaje no puede estar vacío."
-    } else if ((inputMensaje.value).length > 300) {
+        return inputMensaje.value;
+    } else if ((inputMensaje.value).trim().length > 300) {
         styleInput;
         displayBlock;
         alertaMensaje.innerText = "El campo mensaje no puede superar los 300 caracteres.";
+        return inputMensaje.value;
     }
     else {
         inputMensaje.style.cssText = 'border: none';
         alertaMensaje.style.display = 'none';
+        return inputMensaje.value;
     }
 
-}
-
-function enviarFormulario() {
-    validacionNombre();
-    validacionEmail();
-    validacionAsunto();
-    validacionMensaje();
 }
 
 inputNombre.addEventListener('blur', validacionNombre);
@@ -112,8 +115,9 @@ inputMensaje.addEventListener('blur', validacionMensaje);
 // inputNombre.addEventListener('blur', (e) => validacionInput("nombre", e));
 // document.getElementById('boton__enviar').addEventListener('click', console.log("prueba"))
 botonEnviar.addEventListener('click', (e) => {
-    e.preventDefault();
-    enviarFormulario();
+    if (validacionNombre() == "" || validacionEmail() == "" || validacionAsunto() == "" || validacionMensaje() == "") {
+        e.preventDefault();
+    }
 })
 
 // const expresiones = {
